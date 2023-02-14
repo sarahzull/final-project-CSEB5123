9f3d90c98b80811cde5871926ce276226c305c6a
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-02-14 13:10:47 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,8 +18,7 @@
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email')" />
-                {{-- <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus/> --}}
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" value="admin@gmail.com" required autofocus/>
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
@@ -30,7 +29,7 @@
                 <x-text-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="current-password" value="password"/>
+                                required autocomplete="current-password" />
 
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
